9457153f63d3b70a0e29c05cd27f826c4315080a
nimrod silberfeld <nimrod513@gmail.com>
2019-07-16 11:57:48 +0300

    MovieManager.js updated

---
+++ dist/MovieManager.js
@@ -0,0 +1,40 @@
+class MovieManager {
+    constructor() {
+        this.movieData = []
+    }
+
+    async getDataFromDB() {
+        let mv = this.movieData
+        let response = await $.get('http://localhost:3000/movies')
+        if (response) {
+            for (let i of response) {
+                mv.push(i)
+            }
+        }
+
+        return
+    }
+
+    async getTranding(tranding) {
+        let data = await $.get(`http://localhost:3000/city/${tranding}`)
+        data = JSON.parsh(data)
+        let MovieObject = {
+            tranding
+
+        }
+        this.movieData.push(MovieObject)
+
+    }
+
+    saveMovie(movieName) {
+        const movies = this.movieData.find(mv => mv.name === movieName)
+        $.post("/movie/", movies)
+    }
+
+    getSearch(movie)
+
+
+
+
+
+}
