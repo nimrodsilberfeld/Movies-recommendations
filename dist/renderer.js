f1a69340c0a9b6180e6027bc02b178842558f88c
Yonatan Sharoni <yonatan.sharoni@gmail.com>
2019-07-16 11:37:52 +0300

    basic HTML and CSS + boilrplate renderer.js

---
+++ dist/renderer.js
@@ -0,0 +1,11 @@
+class Renderer {
+    constructor(){
+
+    }
+    renderData(){
+        $('#container').empty()
+        const source = $("#template").html()
+        
+        $('#container').append(newHTML)
+    }
+}
