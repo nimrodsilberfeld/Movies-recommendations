0a270ab7a58be2a0550a438164041c653ceec539
davidbool <sdavidxl@gmail.com>
2019-07-17 10:49:32 +0300

    sorting function and rout

--- api.js
+++ api.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const router = express.Router()
 const request = require('request')
 const User = require('./modules/User')
-const sort=require('./modules/Recommend')
+const sort = require('./modules/Recommend')
 
 const key = 'f879f4132d8f332d5be23dee1d085d9f'
 const namekey = 'b6c343c7'
@@ -35,27 +35,31 @@ router.get('/movies/:moviename', function (req, res) {
     })
 })
 
-
+// takes user name and an object {name: "user name"}
+// 
 router.put('/movies/:imdbid', function (req, res) {
     let imdbid = req.params.imdbid
-let user=req.body
+    let user=req.body
     request(`https://api.themoviedb.org/3/find/${imdbid}?api_key=${key}&language=en-US&external_source=imdb_id`, function (err, r, body) {
         const data = JSON.parse(body)
         const id = data.movie_results[0].id
-        // res.sendStatus(data.movie_results[0].id)
+
         request(`https://api.themoviedb.org/3/movie/${id}/similar?api_key=${key}&language=en-US&page=1`, function (err, r, body) {
             const movies = JSON.parse(body)
-            res.send(movies)
-            User.findOne({name:user.name},function(err,d){
-            let list=  sort(d.movies,d.recommendedMovies)
-            
-            User.update({name:data.name},{$set:{recommendedMovies:list}},function(err,x){
-                res.send(list)
+            User.findOne({name: user.name},function(err,d){
+            let list =  sort(movies.results, d.recommendedMovies.results)
+            d.recommendedMovies = list
+            d.save(function(err){
+                if(err){
+                    console.log(err)
+                }
+            })
+            res.send(list)
             })
             })
         })
     })
-})
+
 
 router.post('/user/:username', function (req, res) {
     const data = req.body
--- modules/Recommend.js
+++ modules/Recommend.js
@@ -1,33 +1,44 @@
 const RecommendList = function (list1, list2) {
     let reclist = []
-    let i = 0 
+    let i = 0
     let k = 0
     let numlst = []
-    while(i < list1.length || k < list2.length){
-        list1[i].score = Math.round(list1[i].vote_average)
-        list2[k].score =  Math.round(list2[k].vote_average)
-        if(list1[i].original_title ==list2[k].original_title){
-            list1[i].score ++
+    while (i < list1.length || k < list2.length) {
+        if (list2 = []) {
+            list1[i].score = Math.round(list1[i].vote_average)
             reclist.push(list1[i])
             numlst.push(list1[i].score)
+        }
+        else {
 
+            list1[i].score = Math.round(list1[i].vote_average)
+            list2[k].score = Math.round(list2[k].vote_average)
+
+            if (list1[i].original_title == list2[k].original_title) {
+                list1[i].score++
+                reclist.push(list1[i])
+                numlst.push(list1[i].score)
+            }
+
+            else {
+                reclist.push(list1[i])
+                reclist.push(list2[k])
+                numlst.push(list1[i].score)
+                numlst.push(list1[i].score)
+            }
         }
-        else{
-            reclist.push(list1[i])
-            reclist.push(list2[k])
-            numlst.push(list1[i].score)
-            numlst.push(list1[i].score)
-        }
-                i++
-                k++
+
+
+        i++
+        k++
     }
 
-    finlst = []
-    numlst.sort(function(a, b){return a - b})
-    let j = numlst.length -1
-    while(j >= 0){
-        for(let movie of reclist){
-            if(movie.score == numlst[j]){
+    let finlst = []
+    numlst.sort(function (a, b) { return a - b })
+    let j = numlst.length - 1
+    while (j >= 0) {
+        for (let movie of reclist) {
+            if (movie.score == numlst[j]) {
                 finlst.push(movie)
             }
         }
@@ -36,4 +47,4 @@ const RecommendList = function (list1, list2) {
     return finlst
 }
 
-
+module.exports = RecommendList
